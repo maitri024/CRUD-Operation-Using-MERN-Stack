30e5005aa9e2d84da15f7090ad8ebfebc5666765
maitri024 <83055541+maitri024@users.noreply.github.com>
2023-04-19 12:22:02 +0530

    Add files via upload

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -1,1 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+const designationSchema = new Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  password: { type: String, required: true },
+  email: { type: String, required: true },
+  phone:{type:Number,required:true}
+}, {
+  timestamps: true,
+});
+
+const Designation = mongoose.model('usertable', designationSchema);
+module.exports = Designation;
